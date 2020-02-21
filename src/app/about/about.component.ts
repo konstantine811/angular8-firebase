e4aa6cc179fb6f53961e0487dee743e61231b472
la.abramkin <la.abramkin@kernel.ua>
2020-02-21 18:52:24 +0200

    added firebase config

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+var config = {
+  apiKey: "AIzaSyBdrxqDZWL6XVaL3F8pcdkTBIv1Owj5aok",
+  authDomain: "fir-course-122a2.firebaseapp.com",
+  databaseURL: "https://fir-course-122a2.firebaseio.com",
+  projectId: "fir-course-122a2",
+  storageBucket: "fir-course-122a2.appspot.com",
+  messagingSenderId: "485204055168",
+  appId: "1:485204055168:web:7198568c8972d2eef9c265",
+  measurementId: "G-ESPMJRQJHV"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+const settings = {};
+
+db.settings(settings);
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"]
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    db.doc("courses/RV3FRkLPpxKoJFcPT6ai")
+      .get()
+      .then(snap => console.log(snap.data()));
   }
-
 }
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,40 +1,26 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {tap} from 'rxjs/operators';
-import {Observable} from 'rxjs';
-import {Lesson} from '../model/lesson';
-
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Course } from "../model/course";
+import { tap } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { Lesson } from "../model/lesson";
 
 @Component({
-  selector: 'course',
-  templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  selector: "course",
+  templateUrl: "./course.component.html",
+  styleUrls: ["./course.component.css"]
 })
 export class CourseComponent implements OnInit {
-
   course: Course;
+  dataSource;
 
-  displayedColumns = ['seqNo', 'description', 'duration'];
+  displayedColumns = ["seqNo", "description", "duration"];
 
-
-  constructor(
-    private route: ActivatedRoute) {
-
-
-  }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-
-    this.course = this.route.snapshot.data['course'];
-
-
-
+    this.course = this.route.snapshot.data["course"];
   }
 
-  loadMore() {
-
-  }
-
-
+  loadMore() {}
 }
